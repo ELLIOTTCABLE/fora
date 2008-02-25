9631d205a323b522ce9b705667f3428a91955a94
elliottcable <github.com@elliottcable.com>
2008-02-24 17:58:37 -0900

    Added a rakefile with commands for managing git repos

---
+++ Rakefile.rb
@@ -0,0 +1,28 @@
+def __DIR__; File.dirname(__FILE__); end
+NAME = "fora"
+
+##############################################################################
+# Git
+##############################################################################
+namespace :repo do
+
+  desc "Add all new modifications to repository, indiscriminately"
+  task :add do
+    system "git add *"
+  end
+
+  desc "Fetch new modifications from origin, and rebase"
+  task :rebase do
+    system "git stash; git svn rebase; git stash apply"
+  end
+  
+  desc "commit modifications to the repository"
+  task :commit do
+    system "git commit"
+  end
+  
+  desc "push changes to the origin"
+  task :commit do
+    system "git commit; git push origin"
+  end
+end
