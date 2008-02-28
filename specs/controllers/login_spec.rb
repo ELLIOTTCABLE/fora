9858234a063e6895a5295eeb4d3618b84e75857c
Brent Earl <brent@exitstatusone.com>
2008-02-27 22:29:31 -0600

    Fixed Line Indentations.  Tab spacing altered.

--- specs/controllers/login_spec.rb
+++ specs/controllers/login_spec.rb
@@ -3,31 +3,29 @@
 require 'database'
 
 describe Connection do
-	before(:each) do
+before(:each) do
 		@init = Connection.new
-	end
-		
+		end
 		it "should be assigned ident" do
 			@init.assign_ident("assigned ident")
 			@init.should be_in_ident("assigned ident")
-		end
-
+			end
 		it "should NOT be assigned ident" do
 			@init = Connection.new
 			@init.should_not be_in_ident("assigned ident")
-		end
+			end
 end
 
 describe Database do
-	before(:each) do
+before(:each) do
 		@database = Database.new
-	end
+		end
 		it "should be given connection" do
 			@database.set_connection("given connection")
 			@database.should be_given_connection("given connection")
-		end
+			end
 		it "should NOT be given connection" do
 			@database = Database.new
 			@database.should_not be_given_connection("given connection")
-		end
+			end
 end
