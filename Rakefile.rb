73e90ca62ed17f74971b42d4c1f2ab4433a51b05
elliottcable <github.com@elliottcable.com>
2008-03-02 05:28:46 -0900

    No need for SVN tasks in the rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -52,12 +52,3 @@ unless Gem.cache.search("haml").empty?
     end
   end
 end
-
-##############################################################################
-# SVN
-##############################################################################
-
-desc "Add new files to subversion"
-task :svn_add do
-   system "svn status | grep '^\?' | sed -e 's/? *//' | sed -e 's/ /\ /g' | xargs svn add"
-end
