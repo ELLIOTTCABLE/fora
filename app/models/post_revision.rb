dbe74bd873686c47128c4f9c09ee97a1370850c7
elliottcable <me@elliottcable.name>
2008-03-03 10:05:52 -0900

    More refinements to post revisions, and preparing for adding post tagging

--- app/models/post_revision.rb
+++ app/models/post_revision.rb
@@ -3,6 +3,7 @@ class PostRevision < DataMapper::Base
   property :created_at, :datetime
   
   belongs_to :post
+  validates_presence_of :post
   
   def content=(content)
     if new_record?
--- spec/models/post_revision_spec.rb
+++ spec/models/post_revision_spec.rb
@@ -1,31 +1,54 @@
 require File.join( File.dirname(__FILE__), "..", "spec_helper" )
 
-describe PostRevision do
+shared_examples_for 'a lonely revision' do
   before(:each) do
-    @post = Post.new
-    @post.save
-    @revision = @post.revisions.first
+    @revision = PostRevision.new
   end
   
-  it "should not have modifable content" do
+  it "should not be saveable without an associated post" do
+    (@revision.save).should be_false
+  end
+end
+
+shared_examples_for 'a revision with auto-proxied content' do
+  before(:each) do
+    post = Post.new
+    @revision = post.revisions.last
+    @revision.save
+  end
+  
+  it "should not have modifable content when saved" do
     @revision.content = 'foobar'
     @revision.content.should_not == 'foobar'
   end
-  it "should create a new revision if you attempt to modify the content" do
+  it "should create a new revision if you attempt to modify the content after saving" do
     @revision.content = 'foobar'
     @revision.post.revisions.size.should == 2
   end
   it "should create a new revision with the content if you attempt to modify the content" do
     @revision.content = 'foobar'
+    @revision.post.revisions.last.should_not == @revision
     @revision.post.revisions.last.content.should == 'foobar'
   end
+end
+
+shared_examples_for 'a taggable revision' do
+  it "should be taggable"
+end
+
+describe PostRevision do
+  it_should_behave_like 'a lonely revision'
+  it_should_behave_like 'a revision with auto-proxied content'
+  it_should_behave_like 'a taggable revision'
+  
   # TODO: Clean this spec up, it's confusing
   it "should save with the post" do
     post = @revision.post
-    @revision = @revision.class.new
-    post.revisions << @revision
-    @revision.new_record?.should be_true
+    new_revision = @revision.class.new
+    post.revisions << new_revision
+    
+    new_revision.new_record?.should be_true
     post.save
-    @revision.new_record?.should be_false
+    new_revision.new_record?.should be_false
   end
 end
