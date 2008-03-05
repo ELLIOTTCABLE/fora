00259d84fe635bb69b7bca6de6b3585b8c414676
elliottcable <me@elliottcable.name>
2008-03-04 15:37:54 -0900

    ANOTHER fucking useless commit to test hook bot.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ $TESTING=true
 require 'rubygems'
 require 'merb-core'
 
-
 # TODO: Boot Merb, via the Test Rack adapter
 Merb.start :environment => (ENV['MERB_ENV'] || 'test'),
            :merb_root  => File.join(File.dirname(__FILE__), ".." )
