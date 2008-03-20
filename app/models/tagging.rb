39b390e5382385cfafcdb4c0111632bb0f22834a
elliottcable <git@elliottcable.name>
2008-03-20 08:35:30 -0800

    Removed main-branch stuff from design branch

--- app/models/tagging.rb
+++
@@ -1,4 +0,0 @@
-class Tagging < DataMapper::Base
-  belongs_to :tag
-  belongs_to :post
-end
--- spec/controllers/posts_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.join(File.dirname(__FILE__), "..", 'spec_helper.rb')
-
-describe Posts, "index action" do
-  before(:each) do
-    @controller = Posts.build(fake_request)
-    @controller.dispatch('index')
-  end
-end
